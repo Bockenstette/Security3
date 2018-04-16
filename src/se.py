f6553a58b6040f5001907cbb152429df0e50eccc
Bockenstette <bockenstette_kevin@live.com>
2018-04-16 12:29:55 -0400

    1 & 2abc
    Wrote q1, q2abc and added in folder i missed before

--- src/se.py
+++ src/se.py
@@ -1,8 +1,40 @@
 from Crypto.Cipher import AES
+from hashlib import sha256
+
+import os
+import binascii
+
 from sys import argv
 
-# "Constant" lamda since Python has no constants
-LAMBDA = lambda :256
+def Encrypt(prfk, aesk, index_path, files_path, cipher_path):
+	files = os.listdir(files_path)
+	
+	index = {}
+	ie_index = {}
+	
+	for file in files:
+		index[file] = ReadWords(os.path.join(files_path, file), "r")
+
+	# Populate inverted index
+	for key in index:
+		for word in index[key]:
+			hex = sha256(word.encode("utf-8")).hexdigest()		
+			
+			if hex not in ie_index:
+				ie_index[hex] = []
+			ie_index[hex].append(key)
+			
+	# Write the invertex encrypted index to file
+	to_write = ""
+	for key in ie_index:
+		to_write = to_write + key
+		to_write = to_write + ' ' + ' '.join(ie_index[key])
+		to_write = to_write + '\n'
+	
+	WriteFile(index_path, "w+", to_write)
+	
+	
+
 
 def WriteFile(path, args, content):
 	file = open(path, args)
@@ -18,10 +50,30 @@ def ReadFile(path, args):
 	return content
 # END ReadFile
 
+def ReadWords(path, args):
+	file = open(path, args)
+	content = [word for line in file for word in line.split()]
+	file.close()
+	
+	return content
+# END ReadFile
+
 def main():	
+	LAMBDA = 256
+
 	if argv[1] == "keygen":
+		WriteFile(argv[2], "w+", "")
+		WriteFile(argv[3], "w+", os.urandom(LAMBDA))
+	elif argv[1] == "enc":
 		prf_key = ReadFile(argv[2], "r")
 		aes_key = ReadFile(argv[3], "r")
+		index_path = argv[4]
+		files_path = argv[5]
+		cipher_path = argv[6]
+		
+		Encrypt(prf_key, aes_key, index_path, files_path, cipher_path)
+		
+		
 # END main
 
 if __name__ == "__main__":
